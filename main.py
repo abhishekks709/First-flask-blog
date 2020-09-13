67a265892e7afecba45a98adbe998613cc131902
Abhishek Kumar Sharma <abhishekks709@gmail.com>
2020-09-13 14:00:34 +0530

    add login and session functionality, created dashboard, linked dashboard with database, create edit and delete button but not functioning yet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 import json
@@ -10,12 +10,13 @@ with open('config.json', 'r') as c:
 
 local_server = True
 app = Flask(__name__)
+app.secret_key = 'super-secret key'
 app.config.update(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '465',
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = params['gmail-user'],
-    MAIL_PASSWORD=  params['gmail-password']
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
 )
 mail = Mail(app)
 if(local_server):
@@ -34,6 +35,7 @@ class Contacts(db.Model):
     message = db.Column(db.String(120), nullable=False)
     date = db.Column(db.String(12), nullable=True)
 
+
 class Posts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
@@ -47,8 +49,7 @@ class Posts(db.Model):
 @app.route("/")
 def home():
     posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
-    return render_template('index.html', params=params, posts = posts)
-
+    return render_template('index.html', params=params, posts=posts)
 
 
 @app.route("/post/<string:post_slug>", methods=['GET'])
@@ -56,18 +57,29 @@ def post_route(post_slug):
     post = Posts.query.filter_by(slug=post_slug).first()
     return render_template('post.html', params=params, post=post)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', params=params)
 
-@app.route("/dashboard", methods =['GET','POST'])
-def dashboard():
-    if request.method == 'POST':
-        pass
-        # redirect to admin panel
-    else:
 
-        return render_template('login.html', params=params)
+@app.route("/dashboard", methods=['GET', 'POST'])
+def dashboard():
+    if ('user' in session and session['user'] == params['admin_user']):
+        posts = Posts.query.all()
+        return render_template('dashboard.html', params=params, posts=posts)
+    
+    if request.method == 'POST':
+        username = request.form.get('uname')
+        userpass = request.form.get('pass')
+        if (username == params['admin_user'] and userpass == params['admin_password']):
+            # set the session variable
+            session['user'] = username
+            posts = Posts.query.all()
+            return render_template('dashboard.html', params=params, posts = posts)
+
+    return render_template('login.html', params=params)
+
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -84,11 +96,10 @@ def contact():
         db.session.commit()
         mail.send_message('New message from ' + name,
                           sender=email,
-                          recipients = [params['gmail-user']],
-                          body = message + "\n" + phone
+                          recipients=[params['gmail-user']],
+                          body=message + "\n" + phone
                           )
-    return render_template('contact.html', params = params)
+    return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
-
