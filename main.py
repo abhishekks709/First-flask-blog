14d7dbe4978aceec30756705a5845f657535d6a8
Abhishek Kumar Sharma <abhishekks709@gmail.com>
2020-09-17 18:07:40 +0530

    Added functionality: adding new post, editing & deleting and uploading file

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 from flask_mail import Mail
 import json
+import os
 from datetime import datetime
 
 
+# Json file read control
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
+
 
 local_server = True
 app = Flask(__name__)
 app.secret_key = 'super-secret key'
+app.config['UPLOAD_FOLDER'] = params['upload_location']
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT='465',
@@ -27,6 +32,7 @@ else:
 db = SQLAlchemy(app)
 
 
+# Contact Database for access
 class Contacts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
@@ -36,6 +42,7 @@ class Contacts(db.Model):
     date = db.Column(db.String(12), nullable=True)
 
 
+# Post database object for access
 class Posts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
@@ -46,29 +53,33 @@ class Posts(db.Model):
     img_file = db.Column(db.String(12), nullable=True)
 
 
+# Home page form
 @app.route("/")
 def home():
     posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
     return render_template('index.html', params=params, posts=posts)
 
 
+# View post through post form
 @app.route("/post/<string:post_slug>", methods=['GET'])
 def post_route(post_slug):
     post = Posts.query.filter_by(slug=post_slug).first()
     return render_template('post.html', params=params, post=post)
 
 
+# About page form
 @app.route("/about")
 def about():
     return render_template('about.html', params=params)
 
 
+# Dashboard form
 @app.route("/dashboard", methods=['GET', 'POST'])
 def dashboard():
     if ('user' in session and session['user'] == params['admin_user']):
         posts = Posts.query.all()
         return render_template('dashboard.html', params=params, posts=posts)
-    
+
     if request.method == 'POST':
         username = request.form.get('uname')
         userpass = request.form.get('pass')
@@ -76,11 +87,70 @@ def dashboard():
             # set the session variable
             session['user'] = username
             posts = Posts.query.all()
-            return render_template('dashboard.html', params=params, posts = posts)
+            return render_template('dashboard.html', params=params, posts=posts)
 
     return render_template('login.html', params=params)
 
 
+# Edit post from dashboard
+@app.route("/edit/<string:sno>", methods=['GET', 'POST'])
+def edit(sno):
+    if ('user' in session and session['user'] == params['admin_user']):
+        if (request.method == 'POST'):
+            box_title = request.form.get('title')
+            tline = request.form.get('tline')
+            slug = request.form.get('slug')
+            content = request.form.get('content')
+            img_file = request.form.get('img_file')
+            date = datetime.now()
+            if sno == '0':
+                post = Posts(title=box_title, tag_line=tline, slug=slug,
+                             content=content, img_file=img_file, date=date)
+                db.session.add(post)
+                db.session.commit()
+
+            else:
+                post = Posts.query.filter_by(sno=sno).first()
+                post.title = box_title
+                post.tag_line = tline
+                post.slug = slug
+                post.content = content
+                post.img_file = img_file
+                post.date = date
+                db.session.commit()
+                return redirect('/edit/'+sno)
+        post = Posts.query.filter_by(sno=sno).first()
+        return render_template('edit.html', params=params, post=post)
+
+
+# Uploader control
+@app.route("/uploader", methods=['GET', 'POST'])
+def uploader():
+    if ('user' in session and session['user'] == params['admin_user']):
+        if (request.method == 'POST'):
+            f = request.files['file1']
+            f.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return "Uploaded successfully"
+
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect("/dashboard")
+
+
+@app.route("/delete/<string:sno>")
+def delete(sno):
+    if ('user' in session and session['user'] == params['admin_user']):
+        post = Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+
+    return redirect("/dashboard")
+
+
+# contact form
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if(request.method == 'POST'):
